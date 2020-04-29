5e71bfe7a0b9015e60968d68056e0a78efe63324
= <=>
2020-04-29 00:39:06 -0700

    newest transition

--- MDIA2109Tut6/pages/Contact/index.js
+++ MDIA2109Tut6/pages/Contact/index.js
@@ -1,19 +1,59 @@
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import CustomButton from '../../comps/CustomButton';
 import Header from '../../comps/Header';
 import './contact.css';
 
-const Contact = () => {
+var index = 0;
+const headers_arr = [
+    "Thanks for contacting me!",
+    "Goodbye",
+    "Let me know if you have other questions"
+]
 
+const Contact = () => {
+    //step 1 - create the state variable and the function to update it, and put a default inside.
+    const [header_nametext, setHeader] = useState("Contact me!");
+    const [pageleft, setLeft] = useState("-100%");
+
+  
     useEffect(()=>{
+        //what to do when it's created/born/beginning of the life cycle
         setTimeout(()=>{
-        document.querySelector("#contactpage").style.left = 0;
-        }, 50)
+            setLeft(0);
+        // document.querySelector("#contactpage").style.left = 0;
+        }, 50);
+
+        return ()=>{
+            //what to do when the component dies
+        }
     }, []);
 
- return <div id="contactpage">
-    <Header nametext="Contact me!" fontsize={24}/>
-    <CustomButton text="email" />
+    useEffect(()=>{
+        //what to do when it's created/born/beginning of the life cycle
+    }, []);
+
+    useEffect(()=>{
+        //what to do when a state in the page/component updates
+        // alert("Hi! Header text has changed");
+        setLeft(20);
+        setTimeout(()=>{
+            setLeft(0);
+        }, 500);
+    }, [header_nametext]);
+
+
+
+//step 2 - connect the state variable to the UI
+ return <div id="contactpage" style={{left:pageleft}}>
+    <Header nametext={header_nametext} fontsize={24}/>
+    <CustomButton text="email" onClick={()=>{
+      //step 3 - connect the state function() to an interaction / figure out when you want to update the
+      setHeader(headers_arr[index]);
+      index++;  
+      if(index > headers_arr.length-1){
+          index = 0;
+      }
+    }}/>
 </div>
 }
 
